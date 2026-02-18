7383372d1441804f9b51f98cc9e6ab547d791bb9
yablonovskydima <157278934+yablonovskydima@users.noreply.github.com>
2026-02-19 01:19:22 +0200

    Added method filter, serialization to json and new SortedStats model (#2)

--- log_analyzer/report.py
+++ log_analyzer/report.py
@@ -1,7 +1,10 @@
 import csv
-
+import json
 from rich.console import Console
 from rich.table import Table
+from dataclasses import asdict
+
+from log_analyzer.models import LogStats, SortedStats
 
 console = Console()
 
@@ -10,7 +13,7 @@ def cli_report(stats):
     table_status.add_column("Status code", style="red", justify="right")
     table_status.add_column("Count", style="green", justify="right")
 
-    for status, count in stats["errors_by_status"].items():
+    for status, count in stats.errors_by_status.items():
         table_status.add_row(str(status), str(count))
 
     console.print(table_status)
@@ -19,12 +22,12 @@ def cli_report(stats):
     table_ip.add_column("IP", style="red", justify="right")
     table_ip.add_column("Count", style="green", justify="right")
 
-    for ip, count in stats["top_ip_count"].items():
+    for ip, count in stats.top_ip_count.items():
         table_ip.add_row(str(ip), str(count))
 
     console.print(table_ip)
 
-    for hour, count in stats["requests_per_hour"].items():
+    for hour, count in stats.requests_per_hour.items():
         bar = "█" * (count // 10)
         print(f"{hour.strftime('%H:%M')} | {bar} ({count})")
 
@@ -35,18 +38,22 @@ def save_to_csv(stats, filename):
 
         writer.writerow(["Errors by Status"])
         writer.writerow(["Status", "Count"])
-        for status, count in stats["errors_by_status"].items():
+        for status, count in stats.errors_by_status.items():
             writer.writerow([status, count])
         writer.writerow([])
 
         writer.writerow(["Top IPs By Count"])
         writer.writerow(["IP", "Count"])
-        for ip, count in stats["top_ip_count"].items():
+        for ip, count in stats.top_ip_count.items():
             writer.writerow([ip, count])
         writer.writerow([])
 
         writer.writerow(["Requests By Hours"])
         writer.writerow(["Hour", "Count"])
-        for hour, count in stats["requests_per_hour"].items():
+        for hour, count in stats.requests_per_hour.items():
             writer.writerow([hour.strftime('%Y-%m-%d %H:%M'), count])
         writer.writerow([])
+
+def save_to_json(stats, filename):
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(stats.serialize(), f, indent=4, ensure_ascii=False)
